3a422fa94e2d73469d85c310de8f1190e9f026f4
unknown <kennyman143@gmail.com>
2023-02-24 20:38:15 +0200

    adds hw 18.02

---
+++ HW/18.02.py
@@ -0,0 +1,17 @@
+# Task 1 - Odd\even
+
+number = int(input('Number: '))
+
+if number % 2 == 0:
+    print(True)
+else:
+    print(False)
+
+# Task 2 - Is title?
+
+word = input('Word: ')
+
+if word.istitle():
+    print(True)
+else:
+    print(False)
