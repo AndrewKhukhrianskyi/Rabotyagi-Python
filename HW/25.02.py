39022f7d2470d8c5cfb977c21eb0927421ca0292
unknown <kennyman143@gmail.com>
2023-03-04 02:01:25 +0200

    adds hw 25.02

---
+++ HW/25.02.py
@@ -0,0 +1,18 @@
+# Task 1 - Find Word
+arr = [1, 2, 5, 6, 7, 'dog', 3, 4]
+print(arr[-3])
+
+# Task 2 - Reverse Array
+
+# Part 1 - .reverse()
+arr2 = [1, 2, 3]
+arr2.reverse()
+print(arr2)
+
+# Part 2 - [::-1]
+arr2 = [1, 2, 4]
+print(arr[::-1])
+
+# Part 3 - reversed() - Работает со строками и формирует новое выражение
+arr3 = ['a', 'b', 'c']
+print(''.join(reversed(arr3)))
